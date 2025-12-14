9a979f28a375c87f37e7bd251d8ef89af3fad563
superpatriot133 <sherrilmaurais@gmail.com>
2025-12-14 21:59:59 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd /tmp
+
+wget https://download.max.ru/electron/MAX.deb
+
+sudo apt install ./MAX.deb
+
+wget https://raw.githubusercontent.com/superpatriot133/patriotnix/refs/heads/main/shaman.jpg
+
+sudo apt-get update && sudo apt-get install xwallpaper
+
+xwallpaper --tile shaman.jpg
